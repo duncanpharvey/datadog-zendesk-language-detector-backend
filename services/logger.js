73d70372de0dd9019265a8dcdf970e801353ebec
Duncan Harvey <duncan.harvey@datadoghq.com>
2021-08-09 15:31:50 -0400

    log to file instead of directly to Datadog

--- services/logger.js
+++ services/logger.js
@@ -1,17 +1,11 @@
 const { createLogger, format, transports } = require('winston');
 
-const httpTransportOptions = {
-    host: 'http-intake.logs.datadoghq.com',
-    path: `/v1/input/${process.env.DATADOG_API_KEY}?ddsource=nodejs&service=zendesk-language-detector`,
-    ssl: true
-};
-
 const logger = createLogger({
     level: 'info',
     exitOnError: false,
     format: format.json(),
     transports: [
-        new transports.Http(httpTransportOptions),
+        new transports.File({ filename: `/usr/src/app/logs/zendesk-language-detector.log` }),
     ],
 });
 
